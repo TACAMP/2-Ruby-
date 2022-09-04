85a779fc99cb00d43f812d6326a290c19b57d7c2
EC2 Default User <112141419+TACAMP@users.noreply.github.com>
2022-09-04 07:19:56 +0000

    2-6-3

--- lesson6-1.rb
+++ lesson6-1.rb
@@ -1,8 +1,10 @@
 puts "計算を始めます"
 puts "2つの値を入力してください"
-inputs_key1=gets
-inputs_keys=gets
+
+a=gets.to_i
+b=gets.to_i
+
 puts "計算結果は出力します"
-puts "a*b="+(inputs_key1.to_i*inputs_key2.to_i).to_s
+puts "a*b=#{a*b}"
 puts "計算を終了します"
 
