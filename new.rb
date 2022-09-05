0288197e621d0733d17893f314233c743497e7a5
EC2 Default User <112141419+TACAMP@users.noreply.github.com>
2022-09-05 07:54:25 +0000

    [finish]2-9

--- new.rb
+++ new.rb
@@ -1,15 +1,33 @@
 class Car
-  def run(distance)
+  
+  def move(direction,distance)
+    self.turn(direction)
+    self.run(distance)
+  end
+
+def run(distance)
   puts "車で#{distance}キロ走ります。"
   end
-  
+
   def turn(direction)
   puts "#{direction}に曲がります。"
   end
 end
 
 car=Car.new
+
+car.move("右",5)
+
 car.turn("右")
 
 car=Car.new
 car.run(5)
+
+
+class Car
+  def self.turn(direction)
+    puts "#{direction}に曲がります。"
+  end
+end
+
+Car.turn("右")
